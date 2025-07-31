a6259663f343cd8fd0cbf5405d2ceeee0a1fe9f8
erickjimenezgarcia <erickralph010@gmail.com>
2025-07-31 11:00:34 -0500

    fix: configuración CORS para Vercel

--- api_server.py
+++ api_server.py
@@ -359,7 +359,7 @@ async def websocket_progreso(websocket: WebSocket, filename: str):
 from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Cambia esto a los dominios permitidos
+    allow_origins=["http://localhost:5173"],  # Cambia esto a los dominios permitidos
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
